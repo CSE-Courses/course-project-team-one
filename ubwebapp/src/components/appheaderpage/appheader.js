b0abed431ed34e7b4defdd6050589b765934f52b
jordan <abc@example.com>
2020-09-17 13:10:10 -0400

    #18 Designed header

---
+++ ubwebapp/src/components/appheaderpage/appheader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './appheader.css';
+
+
+const AppHeader = () => {
+    return(
+        <header className="home-header"><h1>
+        <text className="header-text1">UB</text><text className="header-text2">Learns</text>
+        </h1></header>
+    );
+}
+
+export default AppHeader;
