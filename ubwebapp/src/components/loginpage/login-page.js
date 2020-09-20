bdeeb6f777968839a2e9b2b56cedd3bca2df16cd
jordan <abc@example.com>
2020-09-20 19:48:21 -0400

    #25

--- ubwebapp/src/components/loginpage/login-page.js
+++ ubwebapp/src/components/loginpage/login-page.js
@@ -5,7 +5,23 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 function LoginPage() {
     return (
-      <header>Login</header>
+      <div className="login-body">
+          <div className="login-bubble">
+            <div className="bubble-content">
+              <h1 className="head-text"><text>Welcome to </text><text className="bold-blue">UB</text><text className="italic">Learns!</text></h1>
+              <div className="entry">
+                <div>
+                <FontAwesomeIcon icon = "user" size="2x"/>
+                <input type="text" placeholder="  Username"></input></div>
+                <div><br></br><br></br><br></br><br></br><br></br>
+                <FontAwesomeIcon icon = "lock" size="2x"/>
+                <input type="text" placeholder="  Password"></input></div>
+                <br></br><br></br><br></br>
+                <button className="button-login">Login</button>
+              </div>  
+            </div>
+          </div>
+      </div>
     );
   }
   
