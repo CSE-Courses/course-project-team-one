79a90d7db8fc59e42d85ec178935af9d8db1766a
jordan <abc@example.com>
2020-09-22 16:57:30 -0400

    #27

--- ubwebapp/src/components/homepage/home-page.js
+++ ubwebapp/src/components/homepage/home-page.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import queryString from 'query-string';
+import {BrowserRouter as Router, Route, Switch, Link, Redirect} from "react-router-dom";
 import './home-page.css';
 import HomebuttonLayout from './homebutton-layout';
 import ClassSelect from './class-selection';
 
 function HomePage() {
+
+  //If not logged in
+  var studentToken = queryString.parse(window.location.search).q;
+  console.log(studentToken);
+  if(studentToken == null){
+    return(
+      <Redirect to="/login"></Redirect>
+    )
+  }
+
   return (
         <div>
           <ClassSelect></ClassSelect>
--- ubwebapp/src/components/loginpage/login-page.js
+++ ubwebapp/src/components/loginpage/login-page.js
@@ -1,25 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./login-page.css";
 import {BrowserRouter as Router, Route, Switch, Link, Redirect} from "react-router-dom";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 
+
+
 function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  //If correct log in eneted go to homepage, otherwise stay
+  const verify= () => {
+    console.log(username);
+    if (username == "student1" && password== "password"){
+      return "/?q=" + username;
+    }
+    /*Wrong info */
+    return "/login?false"
+  }
+  //Reset values after click
+  const reset = () => {
+    setUsername("");
+    setPassword("");
+  }
+
     return (
       <div className="login-body">
           <div className="login-bubble">
             <div className="bubble-content">
               <h1 className="head-text"><text>Welcome to </text><text className="bold-blue">UB</text><text className="italic">Learns!</text></h1>
-              <div className="entry">
+              <form onSubmit={verify} className="entry">
                 <div>
                 <FontAwesomeIcon icon = "user" size="2x"/>
-                <input type="text" placeholder="  Username"></input></div>
+                <input name="username" onChange={e => setUsername(e.target.value)} type="text" placeholder="  Username" value={username}></input></div>
                 <div><br></br><br></br><br></br><br></br><br></br>
                 <FontAwesomeIcon icon = "lock" size="2x"/>
-                <input classname="text-password" type="password" placeholder="  Password"></input></div>
+                <input name="password" onChange={e => setPassword(e.target.value)} classname="text-password" type="password" placeholder="  Password" value={password}></input></div>
                 <br></br><br></br><br></br>
-                <div className="button-div"><Link to="/"><button className="button-login">Login</button></Link></div>
-              </div>  
+                <div className="button-div"><Link to={verify}><button onClick ={reset} className="button-login" >Login</button></Link></div>
+              </form>  
             </div>
           </div>
       </div>
