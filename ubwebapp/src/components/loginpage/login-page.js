d6a692f2b84e21c65ccc02c30efac5e9b32ff23d
jordan <abc@example.com>
2020-09-21 23:53:31 -0400

    #36

--- ubwebapp/src/components/loginpage/login-page.js
+++ ubwebapp/src/components/loginpage/login-page.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./login-page.css";
+import {BrowserRouter as Router, Route, Switch, Link, Redirect} from "react-router-dom";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 
@@ -17,7 +18,7 @@ function LoginPage() {
                 <FontAwesomeIcon icon = "lock" size="2x"/>
                 <input type="text" placeholder="  Password"></input></div>
                 <br></br><br></br><br></br>
-                <button className="button-login">Login</button>
+                <div className="button-div"><Link to="/"><button className="button-login">Login</button></Link></div>
               </div>  
             </div>
           </div>
