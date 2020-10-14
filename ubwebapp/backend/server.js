469b1c10d71721c902833c0d673d4326d73013d4
wafeliciano20 <firstwillow2@gmail.com>
2020-10-14 16:32:36 -0700

    94 Back-end Starting Files

---
+++ ubwebapp/backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.get('/',(req,res) => res.send('UB Learns alternative'));
+
+app.use('/users',require('./routes/users'));
+
+// Listening to PORT
+app.listen(PORT,() => console.log(`server started on port ${PORT}`));
