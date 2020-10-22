dc99c09b2dbbff13a2ac9b8ece089e3734cd6e4b
Squidmaan <longmatt321@gmail.com>
2020-10-21 20:02:06 -0400

    #118 Add classes to MongoDB database

--- src/components/assignmentspage/assignments-info.js
+++ src/components/assignmentspage/assignments-info.js
@@ -1,21 +1,37 @@
-import React from "react";
-import { Redirect } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
 import AssignmentsPage from "./assignments-page"
+import {BrowserRouter as Router, Route, Switch, Link, Redirect, useLocation} from "react-router-dom";
+import axios from 'axios';
+
+
 
 function AssignmentInformation(){
+    const data = useLocation().data;
 
-    const course = "CSE442 Software Engineering"
-    const assignment = "Assignment: Entire project"
-    const dueDate = "Due: Yesterday!!!"
+const[Classes, setClasses] = useState([]);
+    useEffect(() => {
+    getClasses();
+    }, []);
 
+const getClasses = () =>{
+      axios.get('http://localhost:5000/classes').then(res => console.log(res.data));
+    //axios.get('https://ubwebapp-backend.herokuapp.com/users/').then(res => setUsers(res.data)); //Use this one for public deployment
+}
+
+    //const course = 
+   // const assignment = "Assignment: Entire project"
+    const dueDate = "Due: Yesterday!!!";
+    //Check to see if there is anything there
+    for(const x in Classes){
+        console.log(x)
+    }
     //For every assignment skip about 50 pixels and placce the assignment and grade per course
     //Grow box based on the amount of courses
     return(
         <div  className="assignmentText">
             <div className="assignment-content">
-                <h3>{course}</h3>
+                <h3>{Classes.className}</h3>
                 <br></br>
-                <p>{assignment}</p>
                 <p>{dueDate}</p>
                 <br></br>
             </div>
--- src/components/homepage/class-selection.js
+++ src/components/homepage/class-selection.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-
-
+    
 const ClassSelect = ({username, password}) => {
     return(
         <form>
             <div className="select">
                 <select name="class-select" className="filter-class">
                     <option value="unselected">Select class</option>
-                    <option value="class-1">CSE421-Operating Systems</option>
+                    <option value="class-1">CSE 421-Introduction to Operating Systems</option>
                     <option value="class-2">CSE442-Software Engineering</option>
                     <option value="class-3">CSE426-Blockchain</option>
                     <option value="class-4">DMS333-World Cinema</option>
