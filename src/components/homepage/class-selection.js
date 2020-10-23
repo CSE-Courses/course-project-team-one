db320f73d07a8844f276f14d2e8392f4709cd937
jordan <abc@example.com>
2020-10-22 22:05:46 -0400

    #103 Class selection bar changes depending on user

--- src/components/homepage/class-selection.js
+++ src/components/homepage/class-selection.js
@@ -1,20 +1,40 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
     
-const ClassSelect = ({username, password}) => {
-    return(
-        <form>
-            <div className="select">
-                <select name="class-select" className="filter-class">
-                    <option value="unselected">Select class</option>
-                    <option value="class-1">CSE 421-Introduction to Operating Systems</option>
-                    <option value="class-2">CSE442-Software Engineering</option>
-                    <option value="class-3">CSE426-Blockchain</option>
-                    <option value="class-4">DMS333-World Cinema</option>
-                </select>
-            </div>
-        </form>
-    )
+const ClassSelect = ({id}) => {
+
+    const[user, setUser] = useState([]);
+  
+        useEffect(() => {
+            getUser();
+          }, [id]);
+
+      const getUser = () =>{
+          if(id != ""){
+            axios.get('http://localhost:5000/users/' + id).then(res => setUser((res.data).classes));
+            //axios.get('https://ubwebapp-backend.herokuapp.com/users/').then(res => setUsers(res.data)); //Use this one for public deployment
+          }
+        
+  }
+
+    console.log(user);
+    if(id ==  ""){return(<header>Loading...</header>);}
+    else {
+        //setUser(getUser());
+        return(
+            <form>
+                <div className = "select">
+                    <select className="class-select">
+                        {user.map ((classes)=>
+                            <option value="unselected">{classes.class}</option>
+                        )} 
+                    </select>
+                </div>
+            </form>
+        )
+    }
 }
 
 export default ClassSelect;
--- src/components/homepage/home-page.js
+++ src/components/homepage/home-page.js
@@ -12,23 +12,25 @@ function HomePage() {
 
   const[users, setUsers] = useState([]);
   const data = useLocation().data;
+  var id = "";   //The unique id created in the backend
 
   useEffect(() => {
     getUsers();
   }, []);
 
   const getUsers = () =>{
-        //axios.get('http://localhost:5000/users').then(res => setUsers(res.data));
-      axios.get('https://ubwebapp-backend.herokuapp.com/users/').then(res => setUsers(res.data)); //Use this one for public deployment
+        axios.get('http://localhost:5000/users').then(res => setUsers(res.data));
+      //axios.get('https://ubwebapp-backend.herokuapp.com/users/').then(res => setUsers(res.data)); //Use this one for public deployment
   }
 
    //If incorrect login go back to login, otherwise stay
-   if(users.length > 0){
+   
+  if(users.length > 0){
     var correctInfo = false;
-    console.log(users.length);
     for(var index = 0; index < users.length; index++){
       if(users[index].username == data.username && users[index].password == data.password){
         correctInfo = true;
+        id = users[index]._id;
         break;
       }
     }
@@ -42,7 +44,7 @@ function HomePage() {
   return (
       <div>
         <AppHeader username={data.username} password={data.password}/> 
-        <ClassSelect username={data.username} password={data.password}></ClassSelect>
+        <ClassSelect id = {id}></ClassSelect>
         <HomebuttonLayout username={data.username} password={data.password}></HomebuttonLayout>
       </div>
   );
