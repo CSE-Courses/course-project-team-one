22c079143980f4c86b2d0971cca45ad106b4452d
bhuang22 <bhuang22@buffalo.edu>
2020-11-12 07:20:20 -0500

    #139link actual deadline and file from DB

--- src/components/notificationspage/document.js
+++ src/components/notificationspage/document.js
@@ -1,28 +1,75 @@
-import React from "react";
-import moment from 'moment';
+import React, { useState , useEffect} from "react";
+import {BrowserRouter as Router, Route, Switch, Link, Redirect, useLocation} from "react-router-dom";
+import Noti_link from "./Noti.js";
+import axios from 'axios';
 
 
-const Document = (argc) => {
-    return (
-        <div className="card card-shadow">
-             <h2 className="text-center"> {  argc.title } </h2>{" "}
-            
-            <div>
-                <label htmlFor="Assignment">Name : </label>
-                <a href={argc.assignment_doc1}> Progamming Assignment </a>{" "}
-            </div>
-         
-            <div>           
-                <label htmlFor="due_date"> Due Date: {argc.dueDate} </label>{" "}
-            </div>
-           
-        </div>
+function CourseDocumentsPage() {
+  const data = useLocation().data;
+
+  const[classLinks, setClassLinks] = useState([]);
+  const[Classes, setDuedate] = useState([]);
+
+  useEffect(() => {
+    getClasses();
+    }, []);
+
+     
+    useEffect(() => {
+      getduedate();
+      }, []);
+  
+const getClasses = () =>{
+    axios.get('http://localhost:5000/classes').then(res => setClassLinks(res.data));
+
+    //axios.get('https://ubwebapp-backend.herokuapp.com/classes/').then(res => setClasses(res.data)); //Use this one for public deployment
+  }
+  const getduedate = (argc) =>{
+    axios.get('http://localhost:5000/assg').then(res => setDuedate(res.data));
+    //axios.get('https://ubwebapp-backend.herokuapp.com/classes/').then(res => setClasses(res.data)); //Use this one for public deployment
+}
+
+  //If incorrect login go back to login, otherwise stay
+   if(data == null){
+    return(<Redirect to= "/login"></Redirect>)
+   }
+  const username = data.username;
+  const password = data.password;
+  const currentClass = data.currentClass;
+
+
+  if(classLinks.length < 1){
+    return(
+        <h3 className="Loading">Loading...</h3>
+    )
+  }
+  else{
+    var courseLinks;
+    var duedate ;
+
+    for(var x = 0; x < classLinks.length; x++){
+      if(currentClass == classLinks[x].className  ){
+        courseLinks = classLinks[x].courseDocuments;
+      }
+    }
+    duedate = classLinks[4].assignments[2];
+
+    return(
+      <div>
+              {courseLinks.map ((sections)=>
+                                    <div>
+                                       <Noti_link titles={sections.section[0]} 
+                                       links={sections.section[1]} deadlines={duedate} >
+                      
+                                       </Noti_link>
+                                    </div>
+                                )} 
+       
+      </div>
+      
     );
-};
-
-Document.defaultProps = {
-    assignment_doc1:  "https://piazza.com/class_profile/get_resource/kegjk8a65qz79w/kf8qetmgzg02mk",
-    dueDate :  <p className='grey-text'> {Date.createdAt? moment(Date.createdAt.toDate()).calendar(): "10-19-2020"}</p>,
-};
-
-export default Document;
+  }
+  
+  }
+  
+  export default CourseDocumentsPage;
