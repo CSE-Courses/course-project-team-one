7b44ed7ea943483063175ac6d3e4eddb267e850f
Squidmaan <longmatt321@gmail.com>
2020-12-06 20:36:04 -0500

    #184 Hash users password on login

--- backend/models/user-model.js
+++ backend/models/user-model.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const saltRounds = 10;
 const userSchema = new Schema({
   username: {type: String, required: true},
   password: {type: String, required: true},
@@ -15,6 +16,29 @@ const userSchema = new Schema({
 }, {
   timestamps: true,
 });
+
+userSchema.pre('save',function( next ){
+  var user = this;
+
+  if(user.isModified('password')){
+    bcrypt.genSalt(saltRounds,function(err,salt){
+      if(err) return next(err);
+  
+      bcrypt.hash(user.password,salt, function(err, hash){
+        if(err) return next(err);
+        user.password = hash;
+      })
+    })
+  }else{
+    next()
+  }
+
+});
+
+
+
+
+
 
 const User = mongoose.model('User', userSchema);
 
